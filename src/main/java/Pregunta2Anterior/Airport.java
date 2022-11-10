9d7064571d90623112a12571c67665729fb690a0
ricardoolivaresventura <ricardo.olivares.v@uni.pe>
2022-11-10 09:55:53 -0500

    Answer for the question 2.1

---
+++ src/main/java/Pregunta2Anterior/Airport.java
@@ -0,0 +1,51 @@
+package Pregunta2Anterior;
+
+public class Airport {
+    public static void main(String[] args) {
+        Flight economyFlight = new Flight("1", "Economico");
+        Flight businessFlight = new Flight("2", "Negocios");
+
+        Passenger cesar = new Passenger("Cesar", true);
+        Passenger jessica = new Passenger("Jessica", false);
+
+        businessFlight.addPassenger(cesar);
+        businessFlight.removePassenger(cesar);
+        businessFlight.addPassenger(jessica);
+        economyFlight.addPassenger(jessica);
+
+        System.out.println("Lista de pasajeros de vuelos de negocios:");
+        for (Passenger passenger : businessFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+
+        System.out.println("Lista de pasajeros de vuelos economicos:");
+        for (Passenger passenger : economyFlight.getPassengersList()) {
+            System.out.println(passenger.getName());
+        }
+
+        /**
+         * Pregunta 2.1. Ejecuta el programa y presenta los resultados y exaplica
+         * qué sucede.
+         *
+         * El resultado por consola es:
+         * ------------------------------------------
+         * Lista de pasajeros de vuelos de negocios:
+         * Cesar
+         * Lista de pasajeros de vuelos economicos:
+         * Jessica
+         * ------------------------------------------
+         *
+         * Esto ocurrió porque César es un pasajero VIP, en cambio Jessica no.
+         * Por este motivo, a césar si se le pudo agregar al vuelo comercial,
+         * en cambio a Jessica no se le pudo agregar porque no es VIP.
+         *
+         * Además, vemos que en el código se intentó eliminar a césar del vuelo,
+         * pero como César es VIP, entonces no se le pude agregar.
+         *
+         * También, vemos que a Jessica sí se le pudo agregar, pero al vuelo comercial
+         * dado que no es VIP.
+         *
+         */
+
+    }
+}
---
+++ src/main/java/Pregunta2Anterior/Passenger.java
@@ -0,0 +1,22 @@
+package Pregunta2Anterior;
+
+public class Passenger {
+
+    private String name;
+    private boolean vip;
+
+    public Passenger(String name, boolean vip) {
+        this.name = name;
+        this.vip = vip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+}
+
